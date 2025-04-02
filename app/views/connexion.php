e12fa2946cb7508e5a2da3e19a441458cbbacebc
UnOrdinary <114882529+UnOrdinary95@users.noreply.github.com>
2025-04-02 09:40:38 +0200

    Auth

---
+++ app/views/connexion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form action="?action=connexion" method="POST" autocomplete="on">
+        <h1><a href="./" style="text-decoration: none">⬅️</a> Se connecter</h1>
+        <input type="text" name="identifiant" placeholder="Adresse email ou pseudo"><br><br>
+        <?php if (!empty($erreurs['identifiant'])): ?>
+            <span style="color: red"><?= $erreurs['identifiant'] ?></span><br>
+        <?php endif; ?>
+        <input type="password" name="mdp" placeholder="Mot de passe"><br><br>
+        <?php if (!empty($erreurs['mdp'])): ?>
+            <span style="color: red"><?= $erreurs['mdp'] ?></span><br>
+        <?php endif; ?>
+        <?php if (!empty($erreurs['idmdp'])): ?>
+            <span style="color: red"><?= $erreurs['idmdp'] ?></span><br>
+        <?php endif; ?>
+        <input type="submit" name="connecter" value="Se connecter">
+    </form>
+    <p>Nouveau dans ForaVerse ?
+        <a href="./?action=inscription">Créer un compte</a>
+    </p>
+</body>
---
+++ app/views/inscription.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <form action="?action=inscription" method="POST" autocomplete="on">
+        <h1><a href="./" style="text-decoration: none">⬅️</a> Créer un compte</h1>
+        <input type="text" name="pseudo" placeholder="Pseudo"><br><br>
+        <?php if (!empty($erreurs['pseudo'])): ?>
+            <span style="color: red"><?= $erreurs['pseudo'] ?></span><br>
+        <?php endif; ?>
+        <input type="text" name="email" placeholder="Adresse email"><br><br>
+        <?php if (!empty($erreurs['email'])): ?>
+            <span style="color: red"><?= $erreurs['email'] ?></span><br>
+        <?php endif; ?>
+        <input type="password" name="mdp" placeholder="Mot de passe"><br><br>
+        <?php if (!empty($erreurs['mdp'])): ?>
+            <span style="color: red"><?= $erreurs['mdp'] ?></span><br>
+        <?php endif; ?>
+        <input type="submit" name="creer" value="S'inscrire">
+    </form>
+    <p>Vous avez déjà un compte ?
+        <a href="./?action=connexion">Connectez-vous ici</a>
+    </p>
+</body>
